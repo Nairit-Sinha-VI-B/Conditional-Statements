22d640d8b4d5e64ff6b7c744ba8ef82bda5c8728
Nairit-Sinha-VI-B <nairitsinha@gmail.com>
2026-04-08 21:03:54 +0530

    Add files via upload

---
+++ Odd or Even.py
@@ -0,0 +1,11 @@
+# Number to be given
+
+n = int(input("Enter a number: ")) 
+print("The number is: ", n)
+
+# Check if it is Odd or even...
+
+if n%2==0:
+    print(n, " is an Even Number.")
+else:
+    print(n, " is an Odd Number.")
