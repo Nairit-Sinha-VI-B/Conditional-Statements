f71b8f361dae09a3a321131c59a6cf26a1590c43
Nairit-Sinha-VI-B <nairitsinha@gmail.com>
2026-04-08 20:32:10 +0530

    Add files via upload

---
+++ IF Condition.py
@@ -0,0 +1,13 @@
+# Give variable a value and check if it is positive
+
+number1 = 56
+
+if number1 > 0:
+    print(number1, "The number is positive.")
+
+# Give another variable a value and check if it is positive
+
+number2 = -89
+
+if number2 > 0:
+    print(number2, "The number is positive.")
