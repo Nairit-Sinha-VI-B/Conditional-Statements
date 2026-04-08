cb8c3cd073d818d27b6c20dfbbacfe774f710f74
Nairit-Sinha-VI-B <nairitsinha@gmail.com>
2026-04-08 20:54:41 +0530

    Add files via upload

---
+++ Greater or Smaller.py
@@ -0,0 +1,12 @@
+# Ask number
+
+n = int(input("Enter a number: "))
+
+# Check if it is smaller than 15
+
+if n < 15:
+    print(n, " is smaller than 15. ")
+    print("I'm in the IF Block")
+else:
+    print(n, " is greater than 15.")
+    print("I'm in the ELSE Block")
